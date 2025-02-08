bfb4fcaa9f321463e071ef1a1a76a4e5c19427ac
Xzilleon2 <Xzilleon2@gmail.com>
2025-02-08 09:30:43 +0800

    start again

--- Login.php
+++ Login.php
@@ -3,32 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>On The Go</title>
     <link rel="stylesheet" href="styles.css">
+    <title>ON THE GO</title>
 </head>
 <body>
-    <header class="banner">
-        <div class="banner" id="ban-container">
-            <h3>Logo</h3>
-            <div class="nav">
-                <a href="">Home</a>
-                <a href="">View</a>
-                <a href="">Destinations</a>
-                <a href="">Contact Us</a>
-            </div>
-            <div class="BtnContainer">
-                <button type="button" id="Loginbtn">Log in</button>
-                <button type="button" id="Signupbtn">Sign up</button>
-            </div>
-        </div>
+    <header>
+        <label for="">LOGO</label>
+        <label for="">Help</label>
+        <label for="">Contact Us</label>
+        <label for="">About Us</label>
+        <label for="">Sign up</label>
+        <label for="">Icon</label>
     </header>
+    <div>
 
-    <div class="wrapper">
-        <div class="container">
-            <h1>ON THE GO</h1>
-            <label>"Travels clear the mind, food fills the soul, Peace is the reward "</label>
-            <button type="button" id="Bookingbtn">Book now</button>
-        </div>
     </div>
     
 </body>
