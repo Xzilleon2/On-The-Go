6840ed76b8982fcb471fa732617e47a641e9f82d
Xzilleon2 <Xzilleon2@gmail.com>
2025-02-08 13:13:18 +0800

    semi finish

--- Login.php
+++ Login.php
@@ -10,33 +10,34 @@
     <header>
         <div class="banner">
             <label><h1>LOGO</h1></label>
-            <label>Help</label>
-            <label>Contact Us</label>
-            <label>About Us</label>
-            <label>Sign up</label>
+            <label><a href="">Help</a></label>
+            <label><a href="">Contact Us</a></label>
+            <label><a href="">About Us</a></label>
+            <label><a href="">Sign up</a></label>
             <label>Icon</label>
         </div>
     </header>
-        <div class="form">
-            <H1>WELCOME BACK!</H1>
-            <label for="">Don't have an account? <a href="">Sign up</a></label>
-            <form action="" class="container">
-                <label for="username">Username</label>
-                <input type="text" name="username">
-                <label for="password">Password</label>
-                <input type="password" name="password">
-                <div class="remember">
-                    <label id="remember me">
-                        <input type="checkbox" name="remember" id="remember"> Remember me
-                    </label>
-                    <a href="">Forgot Password?</a>
-                </div>
-                <input type="submit" value="Login">
-            </form>
-        </div>
-        <div class="image">
-
-        </div>
-    
+        <div class="wrapper">
+            <div class="form">
+                <H1>WELCOME BACK!</H1>
+                <label for="">Don't have an account? <a href="">Sign up</a></label>
+                <form action="" class="container" method="POST">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" required>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" required>
+                    <div class="remember">
+                        <label id="remember me">
+                            <input type="checkbox" name="remember" id="remember"> Remember me
+                        </label>
+                        <a href="">Forgot Password?</a>
+                    </div>
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+            <div class="image">
+                <label for="">IMAGE</label>
+            </div>
+        </div>   
 </body>
 </html>
